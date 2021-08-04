b819c0f38af879a6f9f6b87637d3f738ca694fd1
michel jean <michel.jean.uk@gmail.com>
2021-08-04 20:30:46 +0100

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//jshint esversion:6
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
